3f8e6ff270131f896a5c2976d59d34e6c3e2b831
Racibór Dzienisz <dzienisz.racibor@gmail.com>
2025-12-10 13:57:52 +0000

    Changed fields names according to aliases from stg models

--- models/marts/dim_customers.sql
+++ models/marts/dim_customers.sql
@@ -1,7 +1,7 @@
 with customers as (
 
     select
-        id as customer_id,
+        customer_id,
         first_name,
         last_name
 
@@ -12,10 +12,10 @@ with customers as (
 orders as (
 
     select
-        id as order_id,
-        user_id as customer_id,
+        order_id,
+        customer_id,
         order_date,
-        status
+        order_status
 
     from {{ ref('stg_jaffle_shop__orders') }}
 
